18ab11d10193d7738cb06a559ce953b435ab0900
Joshuafrankle <joshuafrankle7@gmail.com>
2021-05-02 03:35:41 +0530

    Added animation and title ✨

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import './App.css';
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth"
+import React, { Component } from 'react'
 
-i
+import ParticlesBg from 'particles-bg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+
+    let config = {
+      num: [1, 5],
+      rps: 0.5,
+      radius: [50, 100],
+      life: [1.5, 3],
+      v: [0.2, 0.5],
+      tha: [-40, 40],
+      alpha: [2, 1],
+      scale: [.1, 0.4],
+      position: "all",
+      color:  ["#833ab4", "#1dfdd8"],
+      cross: "dead",
+      // emitter: "follow",
+      random: 15
+    }
+      
+    return (
+      <>
+        <div class="title_word">Workshop Hub</div>
+        <ParticlesBg num={10} type="custom" config={config}  bg={true} />
+      </>
+    )
+  }
 }
 
 export default App;
