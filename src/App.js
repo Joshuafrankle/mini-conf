4bf886bad478e6e88f9c7739daa627722d3d01eb
Joshuafrankle <joshuafrankle7@gmail.com>
2021-05-02 00:58:17 +0530

    Basic setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth"
+
+i
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
