0e183784f53a94b381e9fe4832496be04ebdc548
Joshuafrankle <joshuafrankle7@gmail.com>
2021-05-02 17:10:18 +0530

    Removed Button shadow

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ class App extends Component {
 
     const CustomButton = withStyles({
       root: {
-        backgroundColor: "'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'",
-        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+        backgroundColor: "",
+        boxShadow: "",
       },
     })(Button);
 
