8839aab4b06b1f35b09ec5289453ea3a69dd66a6
Joshuafrankle <joshuafrankle7@gmail.com>
2021-05-02 22:22:10 +0530

    Made Particles component

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
+import Particles from "./Particles";
 class Home extends Component {
   render() {
     return (
       <>
-        <div className="justify-content-center d-flex mt-2">
-          <img className="img-animation" width="150" height="150" alt="image" />
+      
+        <div className="justify-content-center d-flex mt-2 mb-4">
+          <img className="img-animation" width="120" height="120" alt="image" />
         </div>
 
-        <section></section>
+        <section className="ml-5 container">
+        <iframe
+            src="https://clips.twitch.tv/embed?clip=<slug>&parent=streamernews.example.com"
+            height="500"
+            width="1100"
+            allowfullscreen="<allow full screen>">
+        </iframe>
+        </section>
 
-        <section></section>
+        <section>
+
+        </section>
+        <Particles/>
       </>
     );
   }
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom"
-import ParticlesBg from "particles-bg";
+import Particles from "./Particles"
 
 
 class LandingPage extends Component {
@@ -29,21 +29,6 @@ class LandingPage extends Component {
         boxShadow: "",
       },
     })(Button);
-
-    let config = {
-      num: [1, 3],
-      rps: 0.5,
-      radius: [50, 100],
-      life: [1.5, 3],
-      v: [0.2, 0.5],
-      tha: [-40, 40],
-      alpha: [2, 1],
-      scale: [0.1, 0.4],
-      position: "all",
-      color: ["#833ab4", "#1dfdd8"],
-      cross: "dead",
-      random: 15,
-    };
 
     return (
       <>
@@ -73,7 +58,7 @@ class LandingPage extends Component {
           </motion.div>
         </motion.div>
 
-        <ParticlesBg type="custom" config={config} bg={true} />
+        <Particles />
       </>
     );
   }
