b06bbf07430dfc05e23d66ae42f618b7826c5674
Joshua Frankle <66078274+Joshuafrankle@users.noreply.github.com>
2021-05-04 17:13:04 +0530

    Changed svg size

--- src/Error404.js
+++ src/Error404.js
@@ -4,11 +4,11 @@ function Error404() {
   return (
     <>
       <section
-        className="d-flex align-items-center justify-content-center container"
+        className="align-items-center justify-content-center container"
         style={{ height: "100vh" }}
       >
         <div className="text-center">
-          <img src={LaptopLogo} alt="" width="500" height="400" />
+          <img src={LaptopLogo} alt="" width="600" height="600" />
           <h1>Uh no! Requested URL doesn't exist</h1>
         </div>
       </section>
