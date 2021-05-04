9f53f52bb02bb3e0fac109640dd13dc59f64df2b
Joshua Frankle <66078274+Joshuafrankle@users.noreply.github.com>
2021-05-04 21:01:20 +0530

    Added Firebase Config

--- src/pages/Firebase.js
+++ src/pages/Firebase.js
@@ -3,6 +3,14 @@ import "firebase/auth";
 import "firebase/analytics";
 import "firebase/firestore";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyBLsWGsVrNB7Yu7gk9-Vk_LKbH7Hplfz10",
+  authDomain: "miniconf-d8df7.firebaseapp.com",
+  projectId: "miniconf-d8df7",
+  storageBucket: "miniconf-d8df7.appspot.com",
+  messagingSenderId: "57160507247",
+  appId: "1:57160507247:web:0e734860a7db07dbe4129a",
+  measurementId: "G-V57PDPK6PL",
+};
 
 firebase.initializeApp(firebaseConfig);
