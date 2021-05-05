5fc9d79787787f54cc2267d960b910d7a85aab0f
Joshua Frankle <66078274+Joshuafrankle@users.noreply.github.com>
2021-05-05 13:22:45 +0530

    Completed PrivateRoute

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import firebase from "firebase/app";
 import LinearProgress from "@material-ui/core/LinearProgress";
@@ -6,6 +6,49 @@ import LinearProgress from "@material-ui/core/LinearProgress";
 class PrivateRoute extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(
+      function (user) {
+        if (user) {
+          this.setState({
+            isLoaded: true,
+            user: user,
+          });
+        } else {
+          this.setState({
+            isLoaded: true,
+            user: false,
+          });
+        }
+      }.bind(this)
+    );
+  }
+
+  render() {
+    const { isLoaded, user } = this.state;
+
+    if (!isLoaded) {
+      return (
+        <div
+          className="d-flex align-items-center justify-content-center container"
+          style={{ height: "100vh", width: "100vw" }}
+        >
+          <LinearProgress className="w-25" />
+        </div>
+      );
+    } else {
+      if (user) {
+        const Component = this.props.component;
+        return <Component />;
+      } else {
+        return <Redirect to={{ pathname: "/" }} />;
+      }
+    }
   }
 }
 
