d7e8ff7c49cd8ba75a90099ffbbc741f9b49ea4f
Joshua Frankle <66078274+Joshuafrankle@users.noreply.github.com>
2021-05-06 12:49:16 +0530

    Changed Home layout and Added Carousel.js

---
+++ src/animations/Carousel.js
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+
+function PopUp(props) {
+  return (
+    <>
+      <motion.div
+        whileHover={{ scale: 1.08 }}
+        className="card bg-black text-primary m-5 d-flex align-items-center"
+        style={{
+          width: "14em",
+          boxShadow:
+            "0 8px 16px 0 rgba(0, 0, 0, 0.15), 0 6px 20px 0 rgba(0, 0, 0, 0.16)",
+        }}
+      >
+        <iframe
+          className="col-md-8 col-12 mt-2"
+          src={props.src}
+          style={{ height: "100px", width: "auto" }}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+        <div className="card-body d-flex align-content-between flex-wrap">
+          <h6 className="card-title col-12 p-0 font-weight-bold ">
+            {props.title ? props.title : "Check this out"}
+          </h6>
+          <div className="col-12 p-0">{props.children}</div>
+        </div>
+      </motion.div>
+    </>
+  );
+}
+
+export default PopUp;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import FadeIn from "../animations/FadeIn";
 import firebase from "firebase/app";
 import { useHistory } from "react-router-dom";
 import Particles from "../animations/Particles";
+import Marquee from "react-fast-marquee";
+import PopUp from "../animations/Carousel";
 
 function Home() {
   const history = useHistory();
@@ -17,29 +19,47 @@ function Home() {
   }
   return (
     <>
-      <FadeIn>
-        <div className="p-3">
-          <img className="img-animation" width="70" height="70" alt="" />
-          <Button
-            variant="contained"
-            className="float-right mt-3"
-            color="primary"
-            onClick={logout}
-          >
-            Logout
-          </Button>
-        </div>
-        <div className="container mt-3 justify-content-center d-flex">
+      <div className="p-3">
+        <img className="img-animation" width="70" height="70" alt="" />
+        <Button
+          variant="contained"
+          className="float-right mt-3"
+          color="primary"
+          onClick={logout}
+        >
+          Logout
+        </Button>
+      </div>
+      <div className="container mt-3 justify-content-center ">
+        <div className="row">
           <iframe
-            className=""
-            width="1000"
-            height="450"
+            className="col-md-8 col-12"
+            style={{ height: "80vh" }}
             src="https://www.youtube.com/embed/BFfMbHRdg54"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           ></iframe>
+          <iframe
+            className="col-md-4 col-12"
+            src="https://titanembeds.com/embed/808401706337435709"
+            style={{ height: "auto" }}
+            frameborder="0"
+          ></iframe>
         </div>
-      </FadeIn>
+      </div>
+      <Marquee
+        direction="right"
+        speed={30}
+        gradient
+        gradientWidth={0}
+        gradientColor={[31, 31, 31]}
+      >
+        <PopUp
+          src="https://www.youtube.com/watch?v=gQRX1UlQ0Rs"
+          title="Pattarai's Intro | Who are we? | Special Event | May 2021"
+          text="Check out the intro we made for Pattarai's Special Event. Made with Cinema4D. "
+        />
+      </Marquee>
       <Particles />
     </>
   );
