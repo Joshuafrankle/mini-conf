436a6b39c6a6228a7c362a9bc556441951c5f502
Joshua Frankle <66078274+Joshuafrankle@users.noreply.github.com>
2021-05-16 14:33:51 +0530

    small correction

--- src/pages/Darktheme.js
+++ src/pages/Darktheme.js
@@ -3,7 +3,7 @@ import Button from "@material-ui/core/Button";
 import { useState } from "react";
 
 export default function Toggle() {
-  const [btnImage, setBtnImage] = useState("");
+  const [btnImage, setBtnImage] = useState("🌞");
 
   const handleOnClick = () => {
     if (localStorage.getItem("theme") === "theme-light") {
