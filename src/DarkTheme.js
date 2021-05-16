fbdb86b7a9bc216e10fafaaa310a99cf751f9878
Joshua Frankle <66078274+Joshuafrankle@users.noreply.github.com>
2021-05-16 19:54:41 +0530

    Corrected theme change button

--- src/DarkTheme.js
+++ src/DarkTheme.js
@@ -3,7 +3,9 @@ import Button from "@material-ui/core/Button";
 import { useState } from "react";
 
 export default function Toggle() {
-  const [btnImage, setBtnImage] = useState("🌞");
+  const [btnImage, setBtnImage] = useState(
+    localStorage.getItem("theme") == "theme-light" ? "🌞" : "🌛"
+  );
 
   const handleOnClick = () => {
     if (localStorage.getItem("theme") === "theme-light") {
