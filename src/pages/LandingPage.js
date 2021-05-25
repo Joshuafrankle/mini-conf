c738dda8b1b56ce6035dd7b3f7bafe9588ce8ea7
Joshua Frankle <66078274+Joshuafrankle@users.noreply.github.com>
2021-05-26 01:56:50 +0530

    Extended main-bg height and reduced themeButton size on md display

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -46,10 +46,10 @@ function LandingPage() {
           </p>
         </div>
         <div
-          className="d-flex align-items-center justify-content-center container"
+          className="d-flex align-items-center justify-content-center container main-bg"
           style={{ height: "100vh" }}
         >
-          <div className="text-center main-bg">
+          <div className="text-center">
             <p className="pl-md-5 pl-0 text-pattarai">PATTARAI</p>
             <p className="title-word">Mini Conf</p>
             <Button class="customButton" onClick={signInWithGoogle}>
